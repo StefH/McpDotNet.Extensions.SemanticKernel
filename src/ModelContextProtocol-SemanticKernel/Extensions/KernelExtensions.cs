a8883514a83847e165fbe0cd8adda81492815c4f
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-03-24 10:43:44 +0100

    o

--- src/ModelContextProtocol-SemanticKernel/Extensions/KernelExtensions.cs
+++ src/ModelContextProtocol-SemanticKernel/Extensions/KernelExtensions.cs
@@ -110,6 +110,52 @@ public static class KernelExtensions
         return SseMap[serverName] = sseKernelPlugin;
     }
 
+    /// <summary>
+    /// Creates a Model Content Protocol plugin from an SSE server that contains the specified MCP functions and adds it into the plugin collection.
+    /// </summary>
+    /// <param name="plugins">The plugin collection to which the new plugin should be added.</param>
+    /// <param name="optionsCallback">The <see cref="ModelContextProtocolSemanticKernelSseOptions"/> callback.</param>
+    /// <param name="cancellationToken">The optional <see cref="CancellationToken"/>.</param>
+    /// <returns>A Microsoft.SemanticKernel.KernelPlugin containing the functions provided in functions.</returns>
+    public static Task<KernelPlugin> AddMcpFunctionsFromSseServerAsync(this KernelPluginCollection plugins, Action<ModelContextProtocolSemanticKernelSseOptions> optionsCallback, CancellationToken cancellationToken = default)
+    {
+        Guard.NotNull(plugins);
+        Guard.NotNull(optionsCallback);
+
+        var options = new ModelContextProtocolSemanticKernelSseOptions();
+        optionsCallback(options);
+
+        return AddMcpFunctionsFromSseServerAsync(plugins, options, cancellationToken);
+    }
+
+    /// <summary>
+    /// Creates a Model Content Protocol plugin from an SSE server that contains the specified MCP functions and adds it into the plugin collection.
+    /// </summary>
+    /// <param name="plugins">The plugin collection to which the new plugin should be added.</param>
+    /// <param name="options">The <see cref="ModelContextProtocolSemanticKernelSseOptions"/>.</param>
+    /// <param name="cancellationToken">The optional <see cref="CancellationToken"/>.</param>
+    /// <returns>A Microsoft.SemanticKernel.KernelPlugin containing the functions provided in functions.</returns>
+    public static async Task<KernelPlugin> AddMcpFunctionsFromSseServerAsync(this KernelPluginCollection plugins, ModelContextProtocolSemanticKernelSseOptions options, CancellationToken cancellationToken = default)
+    {
+        Guard.NotNull(plugins);
+        Guard.NotNull(options);
+
+        DataAnnotationOptionsValidator<ModelContextProtocolSemanticKernelSseOptions>.ValidateAndThrow(options);
+
+        var serverName = options.Name;
+
+        if (SseMap.TryGetValue(serverName, out var sseKernelPlugin))
+        {
+            return sseKernelPlugin;
+        }
+
+        var mcpClient = await GetClientAsync(serverName, options.Endpoint, null, options.LoggerFactory, cancellationToken).ConfigureAwait(false);
+        var functions = await mcpClient.MapToFunctionsAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+
+        sseKernelPlugin = plugins.AddFromFunctions(serverName, functions);
+        return SseMap[serverName] = sseKernelPlugin;
+    }
+
     private static async Task<IMcpClient> GetClientAsync(string serverName, string? endpoint, Dictionary<string, string>? transportOptions, ILoggerFactory? loggerFactory, CancellationToken cancellationToken)
     {
         var transportType = !string.IsNullOrEmpty(endpoint) ? TransportTypes.Sse : TransportTypes.StdIo;
---
+++ src/ModelContextProtocol-SemanticKernel/Options/ModelContextProtocolSemanticKernelSseOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelContextProtocol.SemanticKernel.Options;
+
+public class ModelContextProtocolSemanticKernelSseOptions : ModelContextProtocolSemanticKernelOptions
+{
+    /// <summary>
+    /// The base URL of the server.
+    /// </summary>
+    [Required]
+    public string Endpoint { get; init; } = null!;
+}
