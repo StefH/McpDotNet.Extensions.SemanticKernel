549ab77f8c8c5c7e3a288c7e237fcb7f7038ed94
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-03-29 20:17:14 +0100

    x

---
+++ wip/ModelContextProtocolServer.Stdio/StdioServer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol;
+using ModelContextProtocol.Protocol.Types;
+
+namespace ModelContextProtocolServer.Stdio;
+
+public static class StdioServer
+{
+    public static Task RunAsync(string applicationName, string version, params string[] args)
+    {
+        var builder = Host.CreateEmptyApplicationBuilder(settings: new HostApplicationBuilderSettings
+        {
+            ApplicationName = applicationName,
+            Args = args
+        });
+
+        builder.Configuration
+            .AddCommandLine(args)
+            .AddEnvironmentVariables();
+
+        builder.Services
+            .AddMcpServer(o => o.ServerInfo = new Implementation
+            {
+                Name = applicationName,
+                Version = version
+            })
+            .WithStdioServerTransport()
+            .WithToolsFromAssembly();
+
+        var host = builder.Build();
+
+        var cts = new CancellationTokenSource();
+
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            e.Cancel = true;
+            cts.Cancel();
+        };
+
+        AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+        {
+            cts.Cancel();
+        };
+
+        return host.RunAsync(cts.Token);
+    }
+}
