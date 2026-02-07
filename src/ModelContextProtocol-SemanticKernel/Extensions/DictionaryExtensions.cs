ceb73350a4840dee900fc5cdb421d368fffc35e7
Stef Heyenrath <stef.heyenrath@gmail.com>
2026-02-07 07:59:56 +0100

    Upgrade ModelContextProtocol to 0.8.0-preview.1

--- src/ModelContextProtocol-SemanticKernel/Extensions/DictionaryExtensions.cs
+++ src/ModelContextProtocol-SemanticKernel/Extensions/DictionaryExtensions.cs
@@ -4,20 +4,6 @@ namespace ModelContextProtocol.SemanticKernel.Extensions;
 
 internal static class DictionaryExtensions
 {
-#if NETSTANDARD2_0
-    /// <summary>
-    /// Converts a dictionary to a read-only dictionary.
-    /// </summary>
-    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
-    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
-    /// <param name="dictionary">The dictionary to convert.</param>
-    /// <returns>A read-only dictionary containing the same key-value pairs as the input dictionary.</returns>
-    internal static IReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
-    {
-        return new System.Collections.ObjectModel.ReadOnlyDictionary<TKey, TValue>(dictionary);
-    }
-#endif
-
     /// <summary>
     /// Converts a non-generic dictionary to a generic dictionary with string keys and values.
     /// </summary>
